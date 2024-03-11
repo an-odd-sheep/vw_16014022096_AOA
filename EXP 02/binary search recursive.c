14c71c69509bf793b645366cadd3a5f85a65bb5d
vrishank.w <vrishank.w@somaiya.edu>
2024-03-11 22:41:08 +0530

    dazai

--- EXP 02/binary search recursive.c
+++ EXP 02/binary search recursive.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void binary_search(int A[], int key, int imin, int imax) {
     if (imax < imin) {
@@ -18,6 +19,8 @@ void binary_search(int A[], int key, int imin, int imax) {
 
 int main() {
     int n, value;
+    clock_t l1, l2;
+
     printf("Enter value of n: ");
     scanf("%d", &n);
 
@@ -32,7 +35,12 @@ int main() {
     printf("Enter value you want to search for: ");
     scanf("%d", &value);
 
+    l1 = clock();
     binary_search(arr, value, 0, n - 1);
+    l2 = clock();
+
+    double t1 = ((double)(l2 - l1)) / CLOCKS_PER_SEC;
+    printf("TIME : %f \n", t1);
 
     return 0;
 }
